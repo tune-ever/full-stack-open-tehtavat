ac9c6e18e73698e9e1abea21a629453e96a65566
Andreas Achte <andreas.achte@gmail.com>
2022-07-22 12:05:12 +0300

    login

--- osa5/src/App.js
+++ osa5/src/App.js
@@ -27,17 +27,56 @@ const App = () => {
       setPassword('')
     } catch (exception) {
       setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     }
   }
+  
+  const setErrorMessage = () => {
+    console.log("got error")
+  }
 
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input 
+        type="text"
+        value={username}
+        name="Username"
+        onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+        type="password"
+        value={password}
+        name="Password"
+        onChange={({ target}) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+    )
+  
+  if (!user) {
+    return (
+      <div>
+        <h2>Log in to app</h2>
+        {loginForm ()}
+      </div>
+    )
+  }
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {blogs.map(blog =><Blog key={blog.id} blog={blog} />)}
     </div>
   )
 }
+
+
 
 export default App
